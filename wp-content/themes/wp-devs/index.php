e0cfe8df94f564f311d1b99ec50cdd439e59a162
Thiago Campos <=>
2024-01-24 02:25:17 -0300

    marcação index

---
+++ wp-content/themes/wp-devs/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body <?php body_class();?> >
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit officiis possimus ex sequi quo facere perspiciatis rem ratione, in quod illum ab numquam! Aliquam in, non debitis quis aspernatur voluptatum.</p>
+    <div id="page" class="site">
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+                    Logo
+                </div>
+                <div class="searchbox">
+                    Search
+                </div>
+            </section>
+            <section class="menu-area">
+                <nav class="main-menu">
+                    Menu
+                </nav>
+            </section>
+        </header>
+        <!-- -->
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <section class="hero">
+                        Hero
+                    </section>
+                    <section class="services">
+                        Services
+                    </section>
+                    <section class="home-blog">
+                        Blog
+                    </section>
+                </main>
+            </div>
+        </div>
+        <!-- -->
+        <footer class="site-footer">
+            Footer
+        </footer>
+    </div>
+</body>
+</html>
