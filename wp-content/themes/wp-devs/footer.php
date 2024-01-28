457b8cfe5c42d1442e4e811f61586baeb4b3cc62
Thiago Campos <=>
2024-01-28 02:42:54 -0300

    paginas de templates

--- wp-content/themes/wp-devs/footer.php
+++ wp-content/themes/wp-devs/footer.php
@@ -1,7 +1,7 @@
 <footer class="site-footer">
             <div class="container">
                 <div class="copyright">
-                    <p>Copyright 👽 - Todos os direitos reservados</p>
+                    <p>Copyright X - All Rights Reserved</p>
                 </div>
                 <nav class="footer-menu">
                     <?php wp_nav_menu( array( 'theme_location' => 'wp_devs_footer_menu' , 'depth' => 1 )); ?>
--- wp-content/themes/wp-devs/page-home.php
+++ wp-content/themes/wp-devs/page-home.php
@@ -2,7 +2,13 @@
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
-                        <h1>Blog</h1>
+                    <section class="hero">
+                        Hero
+                    </section>
+                    <section class="services">
+                        Services
+                    </section>
+                    <section class="home-blog">
                         <div class="container">
                             <div class="blog-items">
                                 <?php 
@@ -24,7 +30,8 @@
                                         <p>Nothing yet to be displayed!</p>
                                 <?php endif; ?>                                
                             </div>
-                        </div>   
+                        </div>
+                    </section>
                 </main>
             </div>
         </div>
