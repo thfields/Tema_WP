a19066ad8fa422cd137060a512da59975b2ed07d
Thiago Campos <=>
2024-01-30 18:44:39 -0300

    add img theme

--- wp-content/themes/wp-devs/general-template.php
+++ wp-content/themes/wp-devs/general-template.php
@@ -4,6 +4,9 @@ Template Name: General Template
 */
 ?>
 <?php get_header(); ?>
+
+<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
