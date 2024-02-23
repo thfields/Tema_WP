3753217353cefb3745dc0a6d1b24a24c60f31c96
Thiago Campos <=>
2024-02-23 02:49:35 -0300

    [feat] add links and single.php

--- wp-content/themes/wp-devs/page.php
+++ wp-content/themes/wp-devs/page.php
@@ -17,6 +17,10 @@
                                         <?php the_content(); ?>
                                     </article>
                                 <?php
+
+                                if( comments_open() || get_comments_number() ){
+                                    comments_template();
+                                }
                                 endwhile;
                             ?>                                
                         </div>
